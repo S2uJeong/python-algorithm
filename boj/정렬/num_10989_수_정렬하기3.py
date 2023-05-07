efeb34a7cb618e4b8611caf34efd76ac621029be
sj9802 <sj980204@gmail.com>
2023-05-07 14:07:07 +0900

    [정렬] 수 정렬하기3 - 메모리 초과 실패 - 계수 정렬로 해결

---
+++ boj/정렬/num_10989_수_정렬하기3.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+list = [0] * 10001
+
+# 들어오는 수를 계수 리스트에 담는다.
+for i in range(int(input())):
+    x = int(input().strip())
+    list[x] += 1
+
+# 차례대로 출력한다.
+for i in range(1,10001):
+    while list[i] != 0 :
+        print(i)
+        list[i] -= 1
+
+# print(*list, sep='\n')
