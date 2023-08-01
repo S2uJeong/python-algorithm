fb2a832994bf334708fd831ea7275e581ecde1ce
sujeong <sj980204@gmail.com>
2023-08-01 23:12:02 +0900

    [bfs] - 개념 복습

--- book/dfs_bfs/test.py
+++ book/dfs_bfs/test.py
@@ -51,28 +51,4 @@ visited = [False] * 9
 dfs(graph, 1, visited)
 # => 1 2 7 6 8 3 4 5
 
-# ===== bfs =======
-# - dfs 보다 구현이 더 빠르게 동작한다. {dfs:스택,재귀}, {bfs:큐}
-# bfs 예제
-from collections import deque
-
-def bfs(graph, start, visited):
-    queue = deque([start])
-    # 현재 노드 방문처리
-    visited[start] = True
-    # 큐가 빌 때 까지 반복
-    while queue:
-        # 큐에서 하나의 원소를 뽑아 출력
-        v = queue.popleft()
-        print(v, end=' ')
-        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-
-    # 각 노드가 방문된 정보를 리스트 자료형으로 표현
-    visited = [False] * 9
-    # 정의된 BFS 함수 호출
-    bfs(graph, 1, visited)
 
