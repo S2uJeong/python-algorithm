5859ecad3316186c30586327227ab8893c8dbbad
sujeong <sj980204@gmail.com>
2024-01-08 12:21:10 +0900

    [기본] 에라토스테네스 체

---
+++ lecture/basic/소수의개수.py
@@ -0,0 +1,13 @@
+print("[1 ~ 입력하는 값] 범위에서 소수의 개수를 계산하여 출력합니다.")
+in_num = int(input())
+def prime_cnt(p_num):
+    cnt = 0
+    prime_list = [0]*(p_num+1)
+    for i in range(2,p_num +1):
+        if prime_list[i] == 0 :
+            cnt += 1
+            for j in range(i, p_num+1, i):
+                prime_list[j] = 1
+    return cnt
+
+print(prime_cnt(in_num))
