7476376b869043ad15665c3c172ff02545b7efe8
sujeong <sj980204@gmail.com>
2024-01-09 11:31:14 +0900

    [구현] 뒤집은 소수 (복습요망) - 라이브러리 안 쓰고 숫자 뒤집는 로직 - 소수 확인 하는 로직 (반절까지만 확인, 바로 return해서 계산 줄이기)

--- lecture/implement/뒤집은 소수.py
+++ lecture/implement/뒤집은 소수.py
@@ -4,6 +4,15 @@ def reverse_int(p_num) :
     result = int(reversed_str)
     return result
 
+def reverse_int_example(p_num):
+    result = 0
+    while p_num > 0 :
+        tmp = p_num % 10
+        result = result * 10 + tmp
+        p_num //= 10
+    return result
+
+
 # 소수인지 확인하는 함수
 def is_prime(p_num):
    cd_list = []
@@ -14,6 +23,15 @@ def is_prime(p_num):
       return False
    else :
       return True
+
+def is_prime_example(p_num):
+    if p_num == 1 :
+        return False
+    for i in range(2,p_num//2+1): # 반절까지만 탐색해도 결과는 똑같으므로, 또한 자기 자신의 수까지 가지 않게 함으로써 아래 조건식이 성립됨.
+        if p_num % i == 0 :
+            return False
+        else :
+            return True
 
 def first_try():
     print('공백을 기준으로 숫자들을 한 줄에 입력해주세요')
@@ -26,4 +44,3 @@ def first_try():
     return result
 
 print(first_try())
-
