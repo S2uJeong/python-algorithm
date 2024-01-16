16513c2ae24d004803b60bd4e8e4017d53be0760
sujeong <sj980204@gmail.com>
2024-01-16 21:08:01 +0900

    [이분검색] 뮤직비디오 - 다시풀기

--- lecture/fourth/뮤직비디오.py
+++ lecture/fourth/뮤직비디오.py
@@ -7,8 +7,6 @@ def sum_list(lt,rt,p_list):
         res += p_list[i]
     return res
 
-# 이분법을 사용하기 위해 정렬한다.
-songs.sort()
 # lt,rt의 기준이 될 노래의 총 길이를 변수에 대입한다. (최소화하는 대상은 최소용량이므로)
 total = sum(songs)
 res = 1e12
@@ -19,13 +17,16 @@ while lt <= rt :
     mid = (lt + rt) // 2
     print(f'mid : {mid}')
     for idx,lang in enumerate(songs) :
-        if sum + lang > mid :
-            cnt += 1
-            sum = 0
-        sum += lang
-        if idx == len(songs)-1:
-            cnt += 1
-        print(f'{idx}번째 노래 진행중 lang : {lang} , sum:{sum} , cnt : {cnt}')
+        if lang <= mid : # 이거 안 넣어서 버그 났었음
+            if sum + lang > mid:
+                cnt += 1
+                sum = 0
+            sum += lang
+            if idx == len(songs)-1 and sum != 0:
+                 cnt += 1
+            print(f'{idx}번째 노래 진행중 lang : {lang} , sum:{sum} , cnt : {cnt}')
+        else :
+            break
     print(f'mid : {mid}, cnt = {cnt}')
     if mid >= max(songs) and cnt <= T :
         rt = mid - 1
@@ -34,4 +35,5 @@ while lt <= rt :
     else :
         lt = mid + 1
     print(f'lt = {lt}, rt ={rt}')
+
 print(res)
