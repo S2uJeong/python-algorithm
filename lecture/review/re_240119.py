e913ca0326669fdbf03df463ad62b34117a35d39
sujeong <sj980204@gmail.com>
2024-01-19 23:59:38 +0900

    [복습] 뮤직비디오

--- lecture/review/re_240119.py
+++ lecture/review/re_240119.py
@@ -1,4 +1,35 @@
+# fourth > [이분검색] 뮤직비디오.py
 
+song_N, cd_N = map(int, input().split())
+songs = list(map(int, input().split()))
+
+lt, rt = max(songs), sum(songs)
+res = 0
+while lt <= rt:
+    print(f'lt = {lt}, rt = {rt}')
+    mid = (lt + rt) // 2
+    sum = 0
+    cd_cnt = 1
+    for song in songs:
+        if song > mid:
+            cd_cnt = 0
+            print(f'하나의 노래가 용량보다 커요! --- cd_cnt = {cd_cnt}')
+            break
+        else:
+            if sum + song > mid:
+                cd_cnt += 1
+                sum = song
+            else :
+                sum += song
+            print(f'song - {song}, sum = {sum}, cd_cnt = {cd_cnt}')
+
+    if cd_cnt > cd_N:
+        lt = mid + 1
+    else:
+        res = mid
+        rt = mid - 1
+
+print(res)
 
 
 # fourth > [그리디] 증가수열 만들기.py
