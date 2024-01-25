1234b59325f23c5229cbfc0ab4053946684339ec
sujeong <sj980204@gmail.com>
2024-01-25 21:54:40 +0900

    [etc] 회귀를 활용한 이진수 변환

--- lecture/etc/이진수로 변환하기.py
+++ lecture/etc/이진수로 변환하기.py
@@ -1,14 +1,26 @@
-def binary(num) :
-    if num == 0 :
+def binary(num):
+    if num == 0:
         return '0'
-    elif num == 1 :
+    elif num == 1:
         return '1'
 
-    if(num%2 == 0):
-        return binary(num//2) + '0'
-    else :
-        return binary(num//2) + '1'
+    if (num % 2 == 0):
+        return binary(num // 2) + '0'
+    else:
+        return binary(num // 2) + '1'
 
 
 num = int(input())
-print(binary(num))
+
+
+# print(binary(num))
+
+def DFS(n):
+    if n == 0:
+        return
+    else:
+        DFS(n // 2)
+        print(n % 2, end='')
+
+print(DFS(num))
+print('finish')
