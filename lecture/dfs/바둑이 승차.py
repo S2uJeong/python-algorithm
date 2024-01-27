346ac286d08a1a5f87fb19e1cee33528a007a37b
sujeong <sj980204@gmail.com>
2024-01-27 11:07:53 +0900

    [dfs] 회귀함수에서 가지치기 하는 방법

--- lecture/dfs/바둑이 승차.py
+++ lecture/dfs/바둑이 승차.py
@@ -1,16 +1,21 @@
 w, n = map(int,input().split())
 weights = [int(input()) for _ in range(n)]
-max_w = 0
-def DFS(L,sum):
+max_w = -(1e10)
+total = sum(weights)
+def DFS(L,sum, tsum):
     global max_w
+    # 가지치기
+    if sum + (total - tsum) < max_w :
+        return
+    # 조건 맞추기
     if sum > w :
         return
     if L == n :
         if sum > max_w:
             max_w = sum
     else:
-        DFS(L+1, sum)
-        DFS(L + 1, sum +weights[L] ) # L인덱스의 강아지 무게를 포함한다.
+        DFS(L+1, sum, tsum+weights[L])
+        DFS(L + 1, sum+weights[L] , tsum+weights[L]) # L인덱스의 강아지 무게를 포함한다.
 # main
-DFS(0,0)
+DFS(0,0,0)
 print(max_w)
