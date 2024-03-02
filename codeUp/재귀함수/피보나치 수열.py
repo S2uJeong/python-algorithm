e7439d307763b51211e5275e26cb77771b803efc
sujeong <sj980204@gmail.com>
2024-03-02 14:36:49 +0900

    [재귀함수 메모이제이션] - 피보나치 수열

--- codeUp/재귀함수/피보나치 수열.py
+++ codeUp/재귀함수/피보나치 수열.py
@@ -7,4 +7,22 @@ def f(n):
     else :
         return f(n-2) + f(n-1)
 
-print(f(int(input())))
+# print(f(int(input())))
+
+memo = {}
+def use_memo(num):
+    if num in memo:
+        return memo[num]
+
+    if num == 0:
+        memo[0] = 0
+        return memo[0]
+    elif num == 1 :
+        memo[1] = 1
+        return memo[1]
+    else :
+        memo[num] = use_memo(num-1) + use_memo(num-2)
+        return memo[num]
+
+print(use_memo(int(input())))
+
