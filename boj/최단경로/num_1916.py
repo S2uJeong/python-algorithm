516d0a629499e072f42a4ea10aea5a4aa1a49ba1
sujeong <sj980204@gmail.com>
2024-06-03 01:28:39 +0900

    [최단경로] 최소비용 구하기    - 다익스트라    - gold5

---
+++ boj/최단경로/num_1916.py
@@ -0,0 +1,36 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+city = int(input().rstrip())
+bus = int(input().rstrip())
+# 그래프 입력 : s - (e으로 가는데, dist(거리,비용))
+graph = [[] for _ in range(city + 1)] # city 번호: 1 ~ city
+for i in range(bus):
+    s,e,dist = map(int,input().split())
+    graph[s].append((e,dist))
+# 문제
+S, E = map(int,input().split())
+INF = 1e9
+distance = [INF] * (city + 1)
+
+# 다익스트라
+def dijkstra(S):
+    # 시작지점 초기화
+    distanc로e[S] = 0
+    # 최소힙 정의
+    q = []
+    heapq.heappush(q,(0,S)) # 거리, 노드
+    # 시작
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for next,d in graph[now]:
+            cost = dist + d
+            if cost < distance[next]:
+                distance[next] = cost
+                heapq.heappush(q, (cost,next))
+
+dijkstra(S)
+print(distance[E])
